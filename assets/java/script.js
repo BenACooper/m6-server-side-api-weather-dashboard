20fd87cfc011150b97553f8e850e12868491f6c6
Ben Cooper <39800139+BenACooper@users.noreply.github.com>
2023-08-21 22:53:54 -0400

    declared a function to search OpenWeather's 'Call current weather data' using the geocoordinates and a second function to parse the data and return humidity, wind and temp in celcius and farenheit

--- assets/java/script.js
+++ assets/java/script.js
@@ -1,34 +1,45 @@
 //https://openweathermap.org/api/geocoding-api
 //http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
+//https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
 
+var APIKey = "578a74d026d0cdbb7cd49c97bdfad734";
 var searchButtonEl = document.querySelector("#search-button");
 
-function displayTodayWeather() {
-  var rawData = localStorage.getItem("rawWeatherData");
-  console.log(rawData);
+//Declare a function to take the response and store the desired information (temp, wind, humidity, icon) into variables.
+function getWeatherData() {
+    var rawWeatherData = localStorage.getItem("weatherData");
+    console.log(rawWeatherData)
+    
+    if (rawWeatherData) {
+        var weatherData = JSON.parse(rawWeatherData);
+        console.log(weatherData);
 
-  if (rawData) {
-    var weatherData = JSON.parse(rawData);
-    console.log(weatherData);
-
-    var lat = weatherData[0].lat;
-    var lon = weatherData[0].lon;
-
-    console.log(lat);
-    console.log(lon);
-  }
+        var tempKelvin = weatherData.main.temp; //why this response an object?
+        var tempFahrenheit = (tempKelvin - 273.15) * 9/5 + 32;
+        tempFahrenheit = tempFahrenheit.toFixed(2) + "°F" 
+        var tempCelsius = tempKelvin - 273.15
+        tempCelsius = tempCelsius.toFixed(2) + "°C" 
+        var humidity = weatherData.main.humidity; 
+        var wind = weatherData.wind.speed
+        console.log(tempFahrenheit);
+        console.log(tempCelsius);
+        console.log(humidity);
+        console.log(wind);
+    }
 }
 
-function searchApi(cityName) {
-  var APIKey = "578a74d026d0cdbb7cd49c97bdfad734";
-  var queryURL =
-    "http://api.openweathermap.org/geo/1.0/direct?q=" +
-    cityName +
-    ",us&appid=" +
+//Declare a function to take the geo-coordinates and in making an API call to OpenWeather's 'Call current weather data' API and save the respone to local storage for use by getWeatherData()
+function searchByGeoCoords(lat, lon) {
+  var queryByCoordinatesURL =
+    "https://api.openweathermap.org/data/2.5/weather?lat=" +
+    lat +
+    "&lon=" +
+    lon +
+    "&appid=" +
     APIKey;
-  console.log(queryURL);
+  console.log(queryByCoordinatesURL);
 
-  fetch(queryURL)
+  fetch(queryByCoordinatesURL)
     .then(function (response) {
       if (!response.ok) {
         throw response.json();
@@ -36,11 +47,52 @@ function searchApi(cityName) {
       return response.json();
     })
     .then(function (data) {
-      localStorage.setItem("rawWeatherData", JSON.stringify(data));
-      displayTodayWeather();
+      localStorage.setItem("weatherData", JSON.stringify(data));
+      getWeatherData()
     });
 }
 
+//Declare a function to get the geo coordinates from the response to store them in variables and pass them to a searchByGeoCoords function.
+function getGeoCoords() {
+  var rawNameData = localStorage.getItem("nameData");
+  console.log(rawNameData);
+
+  if (rawNameData) {
+    var nameData = JSON.parse(rawNameData);
+    console.log(nameData);
+
+    var lat = nameData[0].lat; //why this response an array?
+    var lon = nameData[0].lon;
+    console.log(lat);
+    console.log(lon);
+
+    searchByGeoCoords(lat, lon);
+  }
+}
+
+//Declare a function to use the stored cityName in making an API call to OpenWeather's Built-in API request by city name, then save the response in local storage for use by getGeoCoords().
+function searchByCityName(cityName) {
+  var queryByNameURL =
+    "http://api.openweathermap.org/geo/1.0/direct?q=" +
+    cityName +
+    ",us&appid=" +
+    APIKey;
+  console.log(queryByNameURL);
+
+  fetch(queryByNameURL)
+    .then(function (response) {
+      if (!response.ok) {
+        throw response.json();
+      }
+      return response.json();
+    })
+    .then(function (data) {
+      localStorage.setItem("nameData", JSON.stringify(data));
+      getGeoCoords();
+    });
+}
+
+//Declare a function that reminds the user to enter a city name if there isn't one, and if there is it will then store the users input in a variable and pass it to the searchByCityName function.
 function getUserInput() {
   var cityName = document.querySelector("#search-input").value;
 
@@ -48,8 +100,9 @@ function getUserInput() {
     console.log("forgot city name");
     return;
   }
-  searchApi(cityName);
+  searchByCityName(cityName);
   console.log(cityName);
 }
 
+//Event listener on search button invokes the getUserInput function.
 searchButtonEl.addEventListener("click", getUserInput);
