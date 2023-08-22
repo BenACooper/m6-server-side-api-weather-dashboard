b3abe9b8d43bad1f9df2982d5fda11dcefa58042
Ben Cooper <39800139+BenACooper@users.noreply.github.com>
2023-08-21 20:52:16 -0400

    scripted search button to fetch response from OpenWeather API

--- assets/java/script.js
+++ assets/java/script.js
@@ -1,31 +1,37 @@
-var searchButton = document.querySelector("#search-button");
+//https://openweathermap.org/api/geocoding-api
+//http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
+
+var searchButtonEl = document.querySelector("#search-button");
 
 function searchApi(cityName) {
-  var apiKey = "578a74d026d0cdbb7cd49c97bdfad734";
+  var APIKey = "578a74d026d0cdbb7cd49c97bdfad734";
   var queryURL =
-    "http://api.openweathermap.org/data/2.5/weather?q=" +
+    "http://api.openweathermap.org/geo/1.0/direct?q=" +
     cityName +
     "&appid=" +
     APIKey;
+  console.log(queryURL);
 
-  getUserInput();
-  fetch(queryURL);
+  fetch(queryURL).then(function (response) {
+    if (!response.ok) {
+      throw response.json();
+    }
+    return response.json();
+  });
 }
 
-function getUserInput(event) {
-  event.preventDefault();
-
+function getUserInput() {
   var cityName = document.querySelector("#search-input").value;
 
   if (!cityName) {
     console.log("forgot city name");
     return;
   }
+  searchApi(cityName);
   console.log(cityName);
-//   searchApi(cityName);
 }
 
-searchButton.addEventListener("click", getUserInput);
+searchButtonEl.addEventListener("click", getUserInput);
 
 //EventListener invokes the handleFormSubmit button passing event.
 //handleSearchFormSubmit invokes the searchApi function passing inputValue.
