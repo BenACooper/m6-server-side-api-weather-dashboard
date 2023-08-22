2b708e8469badb84b39f1b1e251495c178f7279e
Ben Cooper <39800139+BenACooper@users.noreply.github.com>
2023-08-22 00:01:22 -0400

    declared a function to get the forecast data from the API as well

--- assets/java/script.js
+++ assets/java/script.js
@@ -1,45 +1,90 @@
 //https://openweathermap.org/api/geocoding-api
 //http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
 //https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
-
+//api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
 var APIKey = "578a74d026d0cdbb7cd49c97bdfad734";
 var searchButtonEl = document.querySelector("#search-button");
+var todayContainerEl = document.querySelector("#todayContainer");
 
-//Declare a function to take the response and store the desired information (temp, wind, humidity, icon) into variables.
-function getWeatherData() {
-    var rawWeatherData = localStorage.getItem("weatherData");
-    console.log(rawWeatherData)
-    
-    if (rawWeatherData) {
-        var weatherData = JSON.parse(rawWeatherData);
-        console.log(weatherData);
+function searchForecastAPI(lat, lon) {
+  var queryForecastURL =
+    "api.openweathermap.org/data/2.5/forecast?lat=" +
+    lat +
+    "&lon=" +
+    lon +
+    "&appid=" +
+    APIKey;
+  console.log(queryForecastURL);
 
-        var tempKelvin = weatherData.main.temp; //why this response an object?
-        var tempFahrenheit = (tempKelvin - 273.15) * 9/5 + 32;
-        tempFahrenheit = tempFahrenheit.toFixed(2) + "°F" 
-        var tempCelsius = tempKelvin - 273.15
-        tempCelsius = tempCelsius.toFixed(2) + "°C" 
-        var humidity = weatherData.main.humidity; 
-        var wind = weatherData.wind.speed
-        console.log(tempFahrenheit);
-        console.log(tempCelsius);
-        console.log(humidity);
-        console.log(wind);
-    }
+  fetch(queryForecastURL)
+    .then(function (response) {
+      if (!response.ok) {
+        throw response.json();
+      }
+      return response.json();
+    })
+    .then(function (data) {
+      localStorage.setItem("forecastData", JSON.stringify(data));   
+    });
 }
 
-//Declare a function to take the geo-coordinates and in making an API call to OpenWeather's 'Call current weather data' API and save the respone to local storage for use by getWeatherData()
-function searchByGeoCoords(lat, lon) {
-  var queryByCoordinatesURL =
+function displayTodayWeather(tempCelsius, tempFahrenheit, humidity, windSpeed) {
+  //Reset the container each call so only one set of data can display.
+  while (todayContainerEl.firstChild) {
+    todayContainerEl.removeChild(todayContainerEl.firstChild);
+  }
+
+  var todayTempEl = document.createElement("p");
+  todayTempEl.textContent = "Temp: " + tempCelsius + " / " + tempFahrenheit;
+  todayContainerEl.appendChild(todayTempEl);
+
+  var todayWindEl = document.createElement("p");
+  todayWindEl.textContent = "Wind: " + windSpeed + " MPH";
+  todayContainerEl.appendChild(todayWindEl);
+
+  var todayHumidityEl = document.createElement("p");
+  todayHumidityEl.textContent = "Humidity: " + humidity + "%";
+  todayContainerEl.appendChild(todayHumidityEl);
+}
+
+//Declare a function to take the response and store the desired information (temp, wind, humidity, icon) into variables.
+function getTodayData() {
+  var rawWeatherData = localStorage.getItem("weatherData");
+  console.log(rawWeatherData);
+
+  if (rawWeatherData) {
+    var weatherData = JSON.parse(rawWeatherData);
+    console.log(weatherData);
+
+    var tempKelvin = weatherData.main.temp; //why this response an object?
+    var tempFahrenheit = ((tempKelvin - 273.15) * 9) / 5 + 32;
+    tempFahrenheit = tempFahrenheit.toFixed(2) + "°F";
+    var tempCelsius = tempKelvin - 273.15;
+    tempCelsius = tempCelsius.toFixed(2) + "°C";
+    var humidity = weatherData.main.humidity;
+    var windSpeed = weatherData.wind.speed;
+
+    console.log(tempFahrenheit);
+    console.log(tempCelsius);
+    console.log(humidity);
+    console.log(windSpeed);
+
+    displayTodayWeather(tempCelsius, tempFahrenheit, humidity, windSpeed);
+  }
+}
+
+//Declare a function to take the geo-coordinates and in making an API call to OpenWeather's 'Call current weather data' API and save the respone to local storage for use by getTodayData() and getForecastData().
+function searchTodayAPI(lat, lon) {
+  var queryTodayURL =
     "https://api.openweathermap.org/data/2.5/weather?lat=" +
     lat +
     "&lon=" +
     lon +
     "&appid=" +
     APIKey;
-  console.log(queryByCoordinatesURL);
+  console.log(queryTodayURL);
 
-  fetch(queryByCoordinatesURL)
+  fetch(queryTodayURL)
     .then(function (response) {
       if (!response.ok) {
         throw response.json();
@@ -48,11 +93,11 @@ function searchByGeoCoords(lat, lon) {
     })
     .then(function (data) {
       localStorage.setItem("weatherData", JSON.stringify(data));
-      getWeatherData()
+      getTodayData();
     });
 }
 
-//Declare a function to get the geo coordinates from the response to store them in variables and pass them to a searchByGeoCoords function.
+//Declare a function to get the geo coordinates from the response to store them in variables and pass them to the searchTodayAPI & searchForecastAPI functions.
 function getGeoCoords() {
   var rawNameData = localStorage.getItem("nameData");
   console.log(rawNameData);
@@ -66,7 +111,8 @@ function getGeoCoords() {
     console.log(lat);
     console.log(lon);
 
-    searchByGeoCoords(lat, lon);
+    searchTodayAPI(lat, lon);
+    searchForecastAPI(lat, lon);
   }
 }
 
